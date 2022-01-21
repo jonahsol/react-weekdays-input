6e32703a69e20af307efaf1c2291ce5f3b98610a
Axel Milliery <a.milliery@icloud.com>
2022-01-21 15:38:03 +0100

    First commit

---
+++ src/components/DaysOfTheWeekInput/DaysOfTheWeekInput.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const DaysOfTheWeekInput = ({ language, defaultValue, step }) => {
+    return (<p>test</p>)
+} 
+
+export default DaysOfTheWeekInput
---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export { default as DaysOfTheWeekInput } from './DaysOfTheWeekInput'
